db08f79daf2602c31705161ef14f77bc82b26177
aphremthomas <aphremthomas@gmail.com>
2025-03-05 09:09:25 -0500

    changes updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,40 @@ import Footer from "@/components/Footer";
 import bg from '@/images/sava-hr-cover.jpg'
 import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
 
+const punchlineText = "HR is the bridge between people, management, and leadership at\
+                your organization. Ensure you have the right technology,\
+                partnership, and culture strategies in one place to positively\
+                impact employees and business goals." 
+const newJob = () => (
+  <>
+           <div className="image md:w-1/2 2xl:w-1/3">
+               <img className="contain h-48 md:h-72 w-full border-solid border rounded-lg object-cover md:object-fill" src="/images/discuss-compress.jpg"></img>
+             </div>
+             <div className="content md:w-1/2 md:ml-4 2xl:ml-10">
+               <div className="md:hidden mt-8 whysavahr text-2xl">
+               Looking for a job?
+               </div>
+               <div className="whyanswer mt-4 text-base md:text-base 2xl:text-xl">
+               {punchlineText}
+               </div>
+               <a href="/jobs">
+                <Button
+                  sx={{
+                    height: "30px",
+                    marginTop: "10px",
+                    color: "#FFF",
+                    background:'#4cc0b4',
+                    borderColor: "#4cc0b4",
+                    boxShadow:"unset"
+                  }}
+                  variant="contained"
+                >
+                  Learn more
+                </Button>
+               </a>
+             </div>
+ </>
+ )
 
 const whySavaHR = () => (
  <>
@@ -23,15 +57,16 @@ const whySavaHR = () => (
 </>
 )
 export default function Home() {
-
-  const punchlineText = "HR is the bridge between people, management, and leadership at\
-                your organization. Ensure you have the right technology,\
-                partnership, and culture strategies in one place to positively\
-                impact employees and business goals."
   return (
     <div className="home w-full">
       <div className="homebg hidden md:block w-full md:h-full h-1/4 left-0 top-0 fixed bg-cover -z-10 bg-[url('/images/sava-hr-cover.jpg')]"></div>
       <div className="bg-cover h-full md:h-screen md:bg-transparent flex-col md:content-end">
+        <div className="hidden md:flex flex-col items-center">
+          <div className="punchlineWrapper container">
+            <div className="punchline text-xl text-white md:text-lg">Human resource</div>
+            <div className="punchlineDescription text-white w-[600px] md:text-4xl">Empowering People, Elevating Businesses — Your Trusted Partner in HR Excellence.</div>
+          </div>
+        </div>
         <div className="md:hidden [clip-path:ellipse(72%_51%_at_50%_33%)] homebg-mobile w-full h-64 bg-cover bg-[url('/images/sava-hr-cover.jpg')]"></div>
         <div className="waveSection bg-transparent flex-col md:content-end">
           <svg className="hidden md:block" viewBox="0 0 1440 150" xmlns="http://www.w3.org/2000/svg">
@@ -40,12 +75,11 @@ export default function Home() {
               d="M0,255 L80,255 C160,255,320,255,480,222.3 C640,190,800,124,960,83.5 C1120,43,1280,26,1440,50.8 L1440,255 L0,255 Z"
             ></path>
           </svg>
-          <div className="content bg-white flex h-[250px] justify-center">
+          <div className="content bg-white flex h-[35vh] justify-center">
             <div className="container p-12 flex flex-col md:flex-row justify-between items-center relative">
-              <div className="punchline text-xl text-primaryfont md:text-[60px]">Human resource</div>
-              <div className="findjobCard right-0 top-0
-              2xl:absolute 2xl:right-0 2xl:top-[-255px]">
-                <Card sx={{ maxWidth: 345 }}>
+              <div className="hidden md:block findjobCard xl:right-0 xl:top-[-150px]
+              md:absolute md:right-0 2xl:top-[-255px]">
+                <Card className="w-[100%] md:max-w-[350px]">
                   <CardMedia
                     sx={{ height: 240, border:"12px solid #FFF;"}}
                     image="/images/discuss-compress.jpg"
@@ -68,7 +102,8 @@ export default function Home() {
               2xl:absolute 2xl:right-[-150px] 2xl:top-[-25px]">
                 {punchlineText}
               </div> */}
-              <div className="md:hidden  text-center text-primaryfont leading-[45px] mt-6 text-4xl">Build a people strategy that connects life and work.</div>
+              <div className="mobile-punchline-heading md:hidden">Human Resource</div>
+              <div className="md:hidden text-center text-primaryfont leading-[45px] mt-6 text-4xl">Empowering People, Elevating Businesses — Your Trusted Partner in HR Excellence.</div>
             </div>
           </div>
         </div>
@@ -77,7 +112,7 @@ export default function Home() {
 
       <div className="md:hidden mt-24 why p-6 bg-white md:flex md:justify-center">
         <div className="container md:flex md:mt-40 md:justify-center">
-            {whySavaHR()} 
+            {newJob()} 
         </div>
       </div>
       <div className="why_savahr hidden bg-white md:flex flex-col items-center w-full justify-center md:h-[70vh] 2xl:h-[50vh]">
